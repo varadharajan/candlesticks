536462a6497637f62d556131e4cf8f4102386094
Varadharajan Mukundan <srinathsmn@gmail.com>
2017-11-13 13:56:14 +0530

    Introducing ScalaCheck

--- src/test/scala/in/varadharajan/quant/candlesticks/CandleTraitTest.scala
+++ src/test/scala/in/varadharajan/quant/candlesticks/CandleTraitTest.scala
@@ -1,6 +1,8 @@
 package in.varadharajan.quant.candlesticks
 
 import org.joda.time.DateTime
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Properties
 import org.scalatest.{FlatSpec, Matchers}
 
 class CandleTest extends FlatSpec with Matchers {
@@ -16,5 +18,15 @@ class CandleTest extends FlatSpec with Matchers {
   "colour" should "return the colour of the candle based on the open and close" in {
     Candle(100, 110, 90, 105, 1000, DateTime.now()).colour shouldBe Green
     Candle(100, 110, 90, 99, 1000, DateTime.now()).colour shouldBe Red
+  }
+}
+
+
+class CandlePropertyTest extends Properties("Candle"){
+  property("colour") = forAll { (open: Double, high: Double, low: Double, close: Double, volume: Long) =>
+    Candle(open, high, low, close, volume, DateTime.now).colour match {
+      case Red => open > close
+      case Green => open < close
+    }
   }
 }
