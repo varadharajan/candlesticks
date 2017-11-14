4fe6f140de2fdd97846c4cb26a5d6905aa139953
Varadharajan Mukundan <srinathsmn@gmail.com>
2017-11-14 09:23:44 +0530

    Added Englufing patterns

--- src/main/scala/in/varadharajan/quant/candlesticks/Patterns.scala
+++ src/main/scala/in/varadharajan/quant/candlesticks/Patterns.scala
@@ -10,8 +10,12 @@ sealed trait BearishSentiment extends Sentiment
 
 sealed trait Pattern
 
-sealed trait SingleCandlePattern {
+sealed trait SingleCandlePattern extends Pattern {
   def applicable(candle: Candle): Boolean
+}
+
+sealed trait TwoCandlePattern extends Pattern {
+  def applicable(left: Candle, right: Candle): Boolean
 }
 
 object SingleCandlePatterns {
@@ -25,6 +29,17 @@ object SingleCandlePatterns {
   )
 }
 
+object TwoCandlePatterns {
+
+  def apply(left: Candle, right: Candle): Iterable[TwoCandlePattern] =
+    patternList.filter(x => x.applicable(left, right))
+
+  private def patternList: Iterable[TwoCandlePattern] = List(
+    BearishEngulfing
+  )
+}
+
+
 case object Doji extends SingleCandlePattern with UnclearSentiment {
   override def applicable(candle: Candle): Boolean =
     candle.spreadRatio <= 0.1 &&
@@ -36,4 +51,17 @@ case object Hammer extends SingleCandlePattern with UnclearSentiment {
   override def applicable(candle: Candle): Boolean =
     candle.spreadRatio <= 0.1 && (
       (candle.highWick / candle.spreadWithWicks <= 0.3) || (candle.lowWick / candle.spreadWithWicks <= 0.3))
+}
+
+case object BearishEngulfing extends TwoCandlePattern with BearishSentiment {
+  override def applicable(left: Candle, right: Candle) = right.colour.equals(Red) &&
+    right.open >= Math.max(left.open, left.close) &&
+    right.close <= Math.min(left.open, left.close)
+}
+
+case object BullishEngulfing extends TwoCandlePattern with BullishSentiment {
+  override def applicable(left: Candle, right: Candle) = right.colour.equals(Green) &&
+    right.open <= Math.min(left.open, left.close) &&
+    right.close >= Math.max(left.open, left.close)
+
 }
--- src/test/scala/in/varadharajan/quant/candlesticks/SingleCandlePatternTest.scala
+++ src/test/scala/in/varadharajan/quant/candlesticks/SingleCandlePatternTest.scala
@@ -10,6 +10,34 @@ class SingleCandlePatternsTest extends FlatSpec with Matchers {
   }
 
   "apply" should "identify Hammer pattern" in {
-    SingleCandlePatterns(Candle(100,110,97,99, 1000, DateTime.now)) shouldBe List(Hammer)
+    SingleCandlePatterns(Candle(100, 110, 97, 99, 1000, DateTime.now)) shouldBe List(Hammer)
+  }
+}
+
+class BearishEngulfingPatternTest extends FlatSpec with Matchers {
+  "applicable" should "identify Bearish Engulfing pattern" in {
+    val leftCandle = Candle(100, 110, 95, 105, 100, DateTime.now)
+    val rightCandle = Candle(106, 110, 90, 99, 100, DateTime.now)
+    BearishEngulfing.applicable(leftCandle, rightCandle) shouldBe true
+  }
+
+  "applicable" should "not classify as Bearish Engulfing pattern" in {
+    val leftCandle = Candle(100, 110, 95, 105, 100, DateTime.now)
+    val rightCandle = Candle(106, 110, 90, 101, 100, DateTime.now)
+    BearishEngulfing.applicable(leftCandle, rightCandle) shouldBe false
+  }
+}
+
+class BullishEngulfingPatternTest extends FlatSpec with Matchers {
+  "applicable" should "identify Bullish Engulfing pattern" in {
+    val leftCandle = Candle(100, 110, 95, 105, 100, DateTime.now)
+    val rightCandle = Candle(99, 120, 90, 106, 100, DateTime.now)
+    BullishEngulfing.applicable(leftCandle, rightCandle) shouldBe true
+  }
+
+  "applicable" should "not classify as Bullish Engulfing pattern" in {
+    val leftCandle = Candle(100, 110, 95, 105, 100, DateTime.now)
+    val rightCandle = Candle(106, 110, 90, 101, 100, DateTime.now)
+    BullishEngulfing.applicable(leftCandle, rightCandle) shouldBe false
   }
 }
